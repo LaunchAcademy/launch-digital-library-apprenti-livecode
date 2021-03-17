078bf8c189f42d80ac2c9d03056e27d5d5f01120
Heidi Hood <heidih74@gmail.com>
2021-03-17 11:45:10 -0400

    remove data.js file

--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,8 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
-import data from './constants/data';
 
 ReactDOM.render(
-  <App data={data}/>,
+  <App/>,
   document.getElementById('app')
 );
